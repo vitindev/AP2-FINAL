2b680df87593bdaa7062a99b55671a82e9183db4
vitindev <victtorhugo.mendes@gmail.com>
2023-04-22 16:10:08 -0300

    changes

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
+#include <time.h>
 #include "lmath.h"
 
 typedef struct
@@ -12,50 +14,77 @@ typedef struct
 int main()
 {
 
+    setlocale(LC_ALL, "Portuguese");
+
     char cont;
     int function = 0;
     float result = 0;
 
-    do
+    char name[100];
+
+    system("clear");
+
+    printf("\nCalculadora lMath");
+    printf("\n\nGuardamos todos os seus calculos em um arquivo de texto para você não esquecer.");
+    printf("\n\nDigite seu nome: ");
+    scanf("%s", name);
+
+    FILE *file = fopen(strcat(name, "-lmath.txt"), "a");
+
+    if (file == NULL)
+    {
+        printf("Um erro foi encontrado durante a abertura do arquivo.");
+    }
+    else
     {
 
-        system("clear");
-
-        printf("\nCalculadora: ");
-        printf("\nFunções disponíveis para calculo: ");
-        printf("\n 1 - Soma");
-        printf("\n 2 - Subtração");
-        printf("\n 3 - Multiplicação");
-        printf("\n 4 - Divisão");
-        printf("\n 5 - Potenciação");
-        printf("\n 6 - Porcentagem");
-        printf("\n 7 - Moda");
-        printf("\n 8 - Mediana");
-        printf("\n 9 - Media");
-        printf("\n 10 - Fatorial");
-        printf("\n 11 - Combinação");
-        printf("\n 12 - Fibonacci");
-        printf("\n 0 - Para sair do programa");
-
-        printf("\nDigite uma das opções acima: ");
-        scanf("%d", &function);
-
-        if (getFunction(function, &result) != -1)
+        if (ftell(file) == 0)
         {
-
-            printf("\nResultado da operação: %0.2f", result);
-            printf("\nPressione alguma tecla + Enter para continuar\n");
-
-            scanf("%c", &cont);
-            getchar();
+            fprintf(file, "Usúario: %s", name);
+            fprintf(file, "\n\nCalculos efetuados:");
         }
 
-    } while (function != 0);
+        do
+        {
+
+            system("clear");
+
+            printf("\nCalculadora: ");
+            printf("\nFunções disponíveis para calculo: ");
+            printf("\n 1 - Soma");
+            printf("\n 2 - Subtração");
+            printf("\n 3 - Multiplicação");
+            printf("\n 4 - Divisão");
+            printf("\n 5 - Potenciação");
+            printf("\n 6 - Porcentagem");
+            printf("\n 7 - Moda");
+            printf("\n 8 - Mediana");
+            printf("\n 9 - Media");
+            printf("\n 10 - Fatorial");
+            printf("\n 11 - Combinação");
+            printf("\n 12 - Fibonacci");
+            printf("\n 0 - Para sair do programa");
+
+            printf("\nDigite uma das opções acima: ");
+            scanf("%d", &function);
+
+            if (getFunction(function, file, &result) != -1)
+            {
+
+                printf("\nResultado da operação: %g", result);
+                printf("\nPressione alguma tecla + Enter para continuar\n");
+
+                scanf("%c", &cont);
+                getchar();
+            }
+
+        } while (function != 0);
+    }
 
     return 0;
 }
 
-int getFunction(int function, float *result)
+int getFunction(int function, FILE *file, float *result)
 {
 
     system("clear");
@@ -74,6 +103,8 @@ int getFunction(int function, float *result)
 
         *result = sum(sNum.numbers[0], sNum.numbers[1]);
 
+        fprintf(file, "\nSoma entre (%g + %g) resultou em: %g;", sNum.numbers[0], sNum.numbers[1], *result);
+
         break;
 
     case 2:
@@ -85,6 +116,8 @@ int getFunction(int function, float *result)
         scanf("%f", &sNum.numbers[1]);
 
         *result = subtract(sNum.numbers[0], sNum.numbers[1]);
+
+        fprintf(file, "\nSubtração entre (%g - %g) resultou em: %g;", sNum.numbers[0], sNum.numbers[1], *result);
 
         break;
 
@@ -98,6 +131,8 @@ int getFunction(int function, float *result)
 
         *result = multiplier(sNum.numbers[0], sNum.numbers[1]);
 
+        fprintf(file, "\nMultiplicação entre (%g x %g) resultou em: %g;", sNum.numbers[0], sNum.numbers[1], *result);
+
         break;
 
     case 4:
@@ -109,6 +144,8 @@ int getFunction(int function, float *result)
         scanf("%f", &sNum.numbers[1]);
 
         *result = division(sNum.numbers[0], sNum.numbers[1]);
+
+        fprintf(file, "\nDivisão entre (%g / %g) resultou em: %g;", sNum.numbers[0], sNum.numbers[1], *result);
 
         break;
 
@@ -122,6 +159,8 @@ int getFunction(int function, float *result)
 
         *result = pow(sNum.numbers[0], sNum.numbers[1]);
 
+        fprintf(file, "\nPotenciação entre (%g^%g) resultou em: %g;", sNum.numbers[0], sNum.numbers[1], *result);
+
         break;
 
     case 6:
@@ -134,12 +173,16 @@ int getFunction(int function, float *result)
 
         *result = percent(sNum.numbers[0], sNum.numbers[1]);
 
+        fprintf(file, "\n%g% de %g resultou em: %g;", sNum.numbers[1], sNum.numbers[0], *result);
+
         break;
 
     case 7:
 
         structList(&sNum, "Moda");
         *result = mode(sNum.numbers, sNum.tam);
+
+        structListResult(sNum, file, "Moda", result);
 
         break;
 
@@ -148,12 +191,16 @@ int getFunction(int function, float *result)
         structList(&sNum, "Mediana");
         *result = mediana(&sNum.numbers, sNum.tam);
 
+        structListResult(sNum, file, "Mediana", result);
+
         break;
 
     case 9:
 
         structList(&sNum, "Média");
         *result = average(sNum.numbers, sNum.tam);
+
+        structListResult(sNum, file, "Média", result);
 
         break;
 
@@ -163,6 +210,8 @@ int getFunction(int function, float *result)
         scanf("%f", &sNum.numbers[0]);
 
         *result = fatorial(sNum.numbers[0]);
+
+        fprintf(file, "\nFatorial de %g resultou em: %g;", sNum.numbers[0], *result);
 
         break;
 
@@ -176,6 +225,8 @@ int getFunction(int function, float *result)
 
         *result = combination(sNum.numbers[0], sNum.numbers[1]);
 
+        fprintf(file, "\nPossibilidades de combinação de (%g em %g) resultou em: %g;", sNum.numbers[0], sNum.numbers[1], *result);
+
         break;
 
     case 12:
@@ -185,11 +236,15 @@ int getFunction(int function, float *result)
 
         *result = fibonacci(sNum.numbers[0]);
 
+        fprintf(file, "\nO resultado na posição %d na sequencia de fibonacci resultou em: %g;", sNum.numbers[0], *result);
+
         break;
 
     default:
         return -1;
     }
+
+    fcloseall();
 
     return 1;
 }
@@ -207,7 +262,7 @@ void structList(structNumber *sNum, char name[])
 
     for (int i = 0; i < sNum->tam; i++)
     {
-        printf("Digite o número %d para encontrar a mediana: ", i + 1);
+        printf("Digite o número %d para encontrar a %s: ", i + 1, name);
         scanf("%f", &sNum->numbers[i]);
         system("clear");
     }
@@ -215,5 +270,26 @@ void structList(structNumber *sNum, char name[])
     printf("Lista de números: ");
 
     for (int i = 0; i < sNum->tam; i++)
-        printf("%0.2f,", sNum->numbers[i]);
+        printf("%g,", sNum->numbers[i]);
+}
+
+void structListResult(structNumber sNum, FILE *file, char name[], float *result)
+{
+
+    fprintf(file, "\nA %s dos números ", name);
+
+    for (int i = 0; i < sNum.tam; i++)
+    {
+
+        if (i == (sNum.tam - 1))
+        {
+            fprintf(file, "%g", sNum.numbers[i]);
+        }
+        else
+        {
+            fprintf(file, "%g, ", sNum.numbers[i]);
+        }
+    }
+
+    fprintf(file, " é: %g;", *result);
 }
